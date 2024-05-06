ecdf21a40f7f5ebf37d63d76726d70869af4b847
nastyakashko1509 <162901665+nastyakashko1509@users.noreply.github.com>
2024-05-06 19:55:58 +0000

    Add files via upload

---
+++ puzzle.h
@@ -0,0 +1,52 @@
+// puzzle.h
+
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include <QDialog>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+#include <QMouseEvent>
+#include <QMessageBox>
+#include <QTimer>
+#include <QRandomGenerator>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Puzzle; }
+QT_END_NAMESPACE
+
+class Puzzle : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Puzzle(QWidget *parent = nullptr);
+    ~Puzzle();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private:
+    Ui::Puzzle *ui;
+    QGraphicsScene *scene;
+    QGraphicsView *view;
+    QList<QGraphicsPixmapItem *> pieces;
+    QList<QPointF> initialPositions;
+    QGraphicsPixmapItem *selectedPiece = nullptr;
+    QPointF lastMousePos;
+    QPointF initialPos;
+    qreal pieceWidth;
+    qreal pieceHeight;
+    bool isPuzzleLoaded = false; // Инициализация переменной isPuzzleLoaded
+    QVector<QPointF> reservedPositions;
+    QMap<QGraphicsPixmapItem*, QPointF> reservedPositionsMap;
+
+    void loadImage();
+    void shufflePieces();
+    void checkCompleted();
+};
+
+#endif // PUZZLE_H
